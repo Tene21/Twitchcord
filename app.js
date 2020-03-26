1be01ddd00a6c02cbdee5d5becfc301c2a2492e6
Tene21 <craig_chicken@yahoo.co.uk>
2020-03-26 22:19:58 +0000

    Removed deprecated dependency on imgur

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
-const imgur = require('imgur');
 const express = require('express');
 const xhub = require('express-x-hub');
 const bodyParser = require('body-parser');
